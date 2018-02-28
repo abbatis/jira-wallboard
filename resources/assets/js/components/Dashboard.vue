b411874aaa903de924742b30d6c04bca30967cdb
MartijnRijnja <martijn.r@me.com>
2018-02-28 11:14:04 +0100

    [styling-refinements]

--- resources/assets/js/components/Dashboard.vue
+++ resources/assets/js/components/Dashboard.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="dashboard">
-        <h1>{{ msg }}</h1>
+        <!--<h1>{{ msg }}</h1>-->
         <div class="status">
             <div class="status-column status-column--header"
                  v-for="(column, index) in columns"
@@ -46,21 +46,3 @@ export default {
   }
 };
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
